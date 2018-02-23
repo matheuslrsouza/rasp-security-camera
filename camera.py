775701b4de1670abfb27f04ea7cf16b271bf8cc5
Matheus Souza <matheuslrsouza@gmail.com>
2018-02-23 16:21:54 -0300

    change camera to picamera

--- camera.py
+++ camera.py
@@ -1,8 +1,13 @@
 import cv2
+from imutils.video.pivideostream import PiVideoStream
+import imutils
+import time
 
 class VideoCamera(object):
   def __init__(self):
-    self.video = cv2.VideoCapture(0)
+    # self.video = cv2.VideoCapture(0)
+    self.video = PiVideoStream().start()
+    time.sleep(2.0)
 
   def __del__(self):
     self.video_release()
